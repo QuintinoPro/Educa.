643ce6aa17752fa863989303def49920984e151f
quintinopro <lucasquintinotrader@gmail.com>
2023-09-25 19:20:50 -0300

    cursos add

--- js/script.js
+++ js/script.js
@@ -5,3 +5,25 @@ menuBtn.onclick = () => {
     menuBtn.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".course-slider",{
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
