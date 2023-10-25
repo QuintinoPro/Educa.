293ff89fdf3c40cb2fabaa1cf2908ed7fd355f6a
quintinopro <lucasquintinotrader@gmail.com>
2023-10-24 23:47:48 -0300

    atualizações 24/10/2023

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ menuBtn.onclick = () => {
 var swiper = new Swiper(".course-slider",{
     spaceBetween: 20,
     grabCursor: true,
-    loop:true,
+    loop:false,
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
@@ -26,48 +26,4 @@ var swiper = new Swiper(".course-slider",{
           slidesPerView: 3,
         },
     },
-});
-
-var swiper = new Swiper(".teachers-slider",{
-  spaceBetween: 20,
-  grabCursor: true,
-  loop:true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-  },
-});
-
-var swiper = new Swiper(".reviews-slider",{
-  spaceBetween: 20,
-  grabCursor: true,
-  loop:true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  breakpoints: {
-      640: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-  },
 });
